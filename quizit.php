2638fd1f2b868270299b3d522491518ff16f44d1
Timo-alt <6047395@mborijnland.nl>
2025-01-14 14:21:38 +0100

    database toegevoegd

--- quizit.php
+++ quizit.php
@@ -9,6 +9,18 @@
 
 <body>
     <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=quizit", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
     ?>
     <script src="JS/Script.js"></script>
 </body></html>
