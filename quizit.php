6ae6fa2edff22d0b23ddb414dcbd40ea8acaecab
FloSoftwareDev <6050527@mborijnland.nl>
2025-01-14 10:03:51 +0100

    initiated the php document

---
+++ quizit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizit</title>
+    <link rel="stylesheet" href="CSS/Style.css">
+</head>
+
+<body>
+    <?php
+    ?>
+    <script src="JS/Script.js"></script>
+</body></html>
