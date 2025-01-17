ff412ed1df83a4ca5640cc4d249c3598272ae6c9
FloSoftwareDev <6050527@mborijnland.nl>
2025-01-17 13:27:01 +0100

    Update database.php
    changed the database to online

--- database.php
+++ database.php
@@ -1,3 +1,11 @@
 <?php
-$con= new mysqli('localhost','root','','exam')or die("Could not connect to mysql".mysqli_error($con));
+$host = "sql7.freesqldatabase.com"; // Hostname
+$username = "sql7757762";           // Database username
+$password = "wdH4euAp4m";           // Database password
+$database = "sql7757762";           // Database name
+$port = 3306;                       // Port number
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database, $port);
+
 ?>
